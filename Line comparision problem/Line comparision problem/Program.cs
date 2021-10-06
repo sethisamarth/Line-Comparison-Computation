1773e25020b4e8ff242ee9e5abda326bd5e3618c
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-06 15:42:29 +0530

    welcome to line comparision problem

---
+++ Line comparision problem/Line comparision problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line_comparision_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line comparision problem");
+        }
+    }
+}
